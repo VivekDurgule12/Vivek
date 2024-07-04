79dbe30a01d3b09dda9bdaf54520117780aee612
VivekDurgule12 <vdurgule31@gmail.com>
2024-07-04 14:06:36 +0530

    Front page

--- src/components/Viveks.jsx
+++ src/components/Viveks.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react';
 import PageOne from './VivekPages/PageOne';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import PageTwo from './VivekPages/PageTwo'; // Make sure to import PageTwo
+import PageTwo from './VivekPages/PageTwo';
 import PageThree from './VivekPages/PageThree';
 
 // Register the plugin
@@ -16,54 +16,47 @@ function Viveks() {
       scrollTrigger: {
         trigger: gsapRef.current,
         start: "top 20%",
-        end: "",
+        end: "bottom top",
         scrub: 2,
-        // markers:true
       },
       y: -200,
       duration: 1,
-      opacity:0,
-      
+      opacity: 0,
     });
   }, []);
 
- 
   return (
     <>
-      <div className="min-h-screen flex flex-col justify-center items-center ">
-        <div
-          className="md:text-[100px] md:mt-[-10%] md:text-start text-center text-4xl md:mb-[-10%] mb-[20%] "
-        >
-          <div className=''  ref={gsapRef}>
-          <h1 className="relative text-black " >
-  {['V', 'I', 'V', 'E', 'K'].map((char, index) => (
-    <span
-      key={index}
-      className="inline-block hover:text-[#000000a3] transition-all duration-200"
-    >
-      {char}
-    </span>
-  ))}
-</h1>
-<h1 className="relative text-black ">
-  {['D', 'U', 'R', 'G', 'U', 'L', 'E'].map((char, index) => (
-    <span
-      key={index}
-      className="inline-block hover:text-[rgba(0,0,0,0.64)] transition-all duration-200"
-    >
-      {char}
-    </span>
-  ))}
-</h1>
-
-          <hr className='border-t-3 border-gray-700 mt-[90px] mb-11' />
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <div className="text-4xl md:text-[100px] text-center mt-20 md:mt-0 mb-20 md:mb-0">
+          <div ref={gsapRef}>
+            <h1 className="relative text-black">
+              {['V', 'I', 'V', 'E', 'K'].map((char, index) => (
+                <span
+                  key={index}
+                  className="inline-block hover:text-[#000000a3] transition-all duration-200"
+                >
+                  {char}
+                </span>
+              ))}
+            </h1>
+            <h1 className="relative text-black">
+              {['D', 'U', 'R', 'G', 'U', 'L', 'E'].map((char, index) => (
+                <span
+                  key={index}
+                  className="inline-block hover:text-[rgba(0,0,0,0.64)] transition-all duration-200"
+                >
+                  {char}
+                </span>
+              ))}
+            </h1>
+            <hr className="border-t-3 border-gray-700 mt-10 md:mt-[90px] mb-11" />
           </div>
         </div>
-      
       </div>
-      <PageThree className="" />
-      <div className="">
-        <PageOne className="" />
+      <PageThree />
+      <div>
+        <PageOne />
         <PageTwo />
       </div>
     </>
